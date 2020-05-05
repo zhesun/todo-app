97c198dac3b85b8c72c526a935ba53b6354e29f0
Zhe Sun <zhe.sun@hotmail.com>
2020-05-05 21:44:16 +0300

    Use input from Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Input } from "semantic-ui-react";
 
-function App() { 
-
+function App() {
   const [todoState, setTodoState] = useState({
-    todoText:"",
-    todoItems: []
+    todoText: "",
+    todoItems: [],
   });
 
   function addItem() {
     setTodoState({
       todoText: "",
-      todoItems: todoState.todoItems.concat(todoState.todoText)
+      todoItems: todoState.todoItems.concat(todoState.todoText),
     });
-  };
+  }
 
-  const listItems = todoState.todoItems.map((item, i) =>
+  const listItems = todoState.todoItems.map((item, i) => (
     <li key={i}>{item}</li>
-  );
+  ));
 
   return (
     <div className="App">
-      <input type="text" value={todoState.todoText} onChange={(event)=> {
-        console.log(event.target.value); 
-        setTodoState({
-          todoText:event.target.value,
-          todoItems: todoState.todoItems
-        })
-        }}> 
-      </input>
-      <button onClick={addItem}>
-        Add
-      </button>
-  <ul>{listItems}</ul>
+      <Input
+        action={{
+          content: "add",
+          onClick: () => addItem(),
+        }}
+        placeholder="add item..."
+        onChange={(event) => {
+          console.log(event.target.value);
+          setTodoState({
+            todoText: event.target.value,
+            todoItems: todoState.todoItems,
+          });
+        }}
+        value={todoState.todoText}
+      />
+
+      <ul>{listItems}</ul>
     </div>
   );
 }
