fac1d180d88ac486d7643445ad0a99ed97b671ae
Zhe Sun <zhe.sun@hotmail.com>
2020-05-16 22:34:20 +0300

    Add checkbox for items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Input, List } from "semantic-ui-react";
+import { Input, List, Checkbox } from "semantic-ui-react";
 
 function App() {
   const [todoState, setTodoState] = useState({
@@ -11,13 +11,38 @@ function App() {
   function addItem() {
     setTodoState({
       todoText: "",
-      todoItems: todoState.todoItems.concat(todoState.todoText),
+      todoItems: todoState.todoItems.concat({
+        text: todoState.todoText,
+        isDone: false,
+      }),
     });
   }
 
-  const listItems = todoState.todoItems.map((item, i) => (
-    <List.Item>{item}</List.Item>
-  ));
+  const listItems = todoState.todoItems.map((item, i) => {
+    let decoration = {};
+    if (item.isDone === true) {
+      decoration = { textDecoration: "line-through" };
+    }
+
+    return (
+      <List.Item>
+        <Checkbox
+          style={decoration}
+          label={item.text}
+          onChange={(event, data) => {
+            todoState.todoItems[i] = {
+              text: todoState.todoItems[i].text,
+              isDone: !todoState.todoItems[i].isDone,
+            };
+            setTodoState({
+              todoText: todoState.todoText,
+              todoItems: todoState.todoItems,
+            });
+          }}
+        />
+      </List.Item>
+    );
+  });
 
   return (
     <div className="App">
